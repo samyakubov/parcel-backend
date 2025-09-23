c8d3c49c12d6b11db9bc6a2a9258e3d1d8e47e72
samyakubov <75036399+samyakubov@users.noreply.github.com>
2025-09-23 16:25:12 -0400

    more bug fixes

--- endpoint_handlers/property_search/helper_functions/get_job_filings.py
+++ endpoint_handlers/property_search/helper_functions/get_job_filings.py
@@ -4,7 +4,7 @@ from logger_config import logger
 
 def get_job_filings(bbl:str):
     try:
-        job_filings_df = db.execute("SELECT filing_reason, job_filing_number, work_type, applicant_license_number, permittee_s_license_type, applicant_first_name, applicant_last_name, applicant_business_name, applicant_business_address, work_permit, approved_date, issued_date, job_description, expired_date, estimated_job_costs FROM PulledPermits WHERE bbl = ?", [bbl])
+        job_filings_df = db.execute_df("SELECT doc, jobdescription, ownersphone, jobtype, jobstatus, jobstatusdescrp FROM dobjobs WHERE bbl = ?", [bbl])
 
         if job_filings_df.empty:
             return []
--- endpoint_handlers/property_search/search_by_property_address.py
+++ endpoint_handlers/property_search/search_by_property_address.py
@@ -38,7 +38,7 @@ def search_by_property_address(address: str):
                     "previous_owners": previous_owner_data,
                 },
                 "records": transactions_df.sort_values(by="recordedfiled", ascending=False).to_dict(orient="records"),
-                # "job_filings": get_job_filings(transactions_df.iloc[0].bbl),
+                "job_filings": get_job_filings(transactions_df.iloc[0].bbl),
                 "violations": get_violation_data(transactions_df.iloc[0].bbl),
                 "complaints": get_complaint_data(address),
                 "zoning": get_zoning_data(transactions_df.iloc[0].bbl),
